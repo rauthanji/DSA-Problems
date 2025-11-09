bde10418b8ffc5d036e5449ac6b01cb2393e01dc
rauthanji <rauthanvimal@gmail.com>
2025-11-09 21:08:14 +0530

    3rd commit

---
+++ Link-List/DetectCycle.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL || head->next==NULL) return false;
+        ListNode *t1 = head;
+        ListNode *t2 = head;
+        while(t1!= NULL && t1->next!=NULL){
+            t1 = t1->next->next;
+            t2 = t2->next;
+            if(t1==t2) return true;
+        }
+        return false;
+    }
+};
