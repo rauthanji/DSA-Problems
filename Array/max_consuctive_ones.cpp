15c2015e82d9af28ba188abac6c2e0bafaa986ce
rauthanji <rauthanvimal@gmail.com>
2025-11-28 21:07:42 +0530

    commit

---
+++ Array/max_consuctive_ones.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n = nums.size();
+        int count= 0;
+        int maxCount=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                count++;
+                if(count>maxCount){
+                    maxCount=count;
+                }
+            }else{
+                count=0;
+            }
+
+
+            
+        }
+        return maxCount;
+    }
+};
