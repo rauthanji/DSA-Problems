fb229c702fbe954e54f59d83df55b83e6218b1b3
rauthanji <rauthanvimal@gmail.com>
2025-12-09 13:01:54 +0530

    commit

---
+++ 2D Array/matrix_sum.cpp
@@ -0,0 +1,13 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    void Addition(vector<vector<int>>& matrixA, vector<vector<int>>& matrixB) {
+        vector<vector<int>> temp;
+        for(int i=0;i<matrixA.size();i++){
+            for(int j=0;j<matrixB.size();j++){
+                matrixA[i][j]=matrixA[i][j]+matrixB[i][j];
+            }
+        }
+    }
+};
